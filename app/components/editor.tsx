9ae728dd598a257201151889e39ce7d49d951ed9
Masaki Yatsu <yatsu@yatsu.info>
2023-08-13 22:33:03 +0900

    Add basic plugins

---
+++ app/components/editor.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef, useState } from "react";
+import { ClientOnly } from "remix-utils";
+
+import "@mdxeditor/editor/style.css";
+import type { MDXEditorMethods } from "@mdxeditor/editor";
+
+const LazyMDXEditor = React.lazy(() =>
+  import("@mdxeditor/editor").then((mod) => ({ default: mod.MDXEditor }))
+);
+
+export default function Editor() {
+  const [listsPlugin, setListsPlugin] = useState<any>(null);
+  const [quotePlugin, setQuotePlugin] = useState<any>(null);
+  const [headingsPlugin, setHeadingPlugin] = useState<any>(null);
+  const [linkPlugin, setLinkPlugin] = useState<any>(null);
+  const [codeBlockPlugin, setCodeBlockPlugin] = useState<any>(null);
+  const [thematicBreakPlugin, setThematicBreakPlugin] = useState<any>(null);
+  const [markdownShortcutPlugin, setMarkdownShortcutPlugin] =
+    useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/lists");
+      setListsPlugin(mod.listsPlugin());
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/quote");
+      setQuotePlugin(mod.quotePlugin());
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/headings");
+      setHeadingPlugin(mod.headingsPlugin());
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/link");
+      setLinkPlugin(mod.linkPlugin());
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/codeblock");
+      setCodeBlockPlugin(
+        mod.codeBlockPlugin({
+          defaultCodeBlockLanguage: "txt",
+        })
+      );
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/thematic-break");
+      setThematicBreakPlugin(mod.thematicBreakPlugin());
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const mod = await import("@mdxeditor/editor/plugins/markdown-shortcut");
+      setMarkdownShortcutPlugin(mod.markdownShortcutPlugin);
+    })();
+  }, []);
+
+  const ref = useRef<MDXEditorMethods>(null);
+
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <ClientOnly>
+        {() => (
+          <>
+            <LazyMDXEditor
+              ref={ref}
+              markdown="# Hello world"
+              contentEditableClassName="prose"
+              plugins={[
+                listsPlugin,
+                quotePlugin,
+                headingsPlugin,
+                linkPlugin,
+                codeBlockPlugin,
+                thematicBreakPlugin,
+                markdownShortcutPlugin, // e.g. `# heading` => <h1><span>heading</span></h1>
+              ].filter((p) => p)}
+            />
+            <button onClick={() => console.log(ref.current?.getMarkdown())}>
+              Get markdown
+            </button>
+          </>
+        )}
+      </ClientOnly>
+    </React.Suspense>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,14 @@
-import React from "react";
 import type { V2_MetaFunction } from "@remix-run/node";
 
-import '@mdxeditor/editor/style.css';
+import Editor from "~/components/editor";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "MDXEditor Example" },
+    { name: "description", content: "MDXEditor Example" },
   ];
 };
 
-const LazyMDXEditor = React.lazy(() => import("@mdxeditor/editor").then(mod => ({default:mod.MDXEditor})));
-
 export default function Index() {
-  return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <LazyMDXEditor markdown="Hello world" />
-    </React.Suspense>      
-  );
+  return <Editor />
 }
